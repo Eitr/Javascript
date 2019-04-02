d6c1ebeb400f8983c4eb9d5c1f0435b545226811
Michael Short <Michael@ai-sys.com>
2019-04-02 10:22:10 -0500

    Update FractalTree.js

--- FractalTree.js
+++ FractalTree.js
@@ -1,84 +1,111 @@
+/* Michael Short 2017 - Recursive Fractal Tree Generator */
 
+// Graphics elments
+let canvas = getElem("canvas");
+let sidebar = getElem("sidebar");
+let showButton = getElem("showPanel");
+let g2d = canvas.getContext("2d");
+let r,g,b;
+
+// List of label names
+const minMaxLabels = ["branch","size","scale","angle"];
+const otherLabels = ["locX","locY","startLength","startWidth","colorRange","fps","redraw"];
+
+// Label 
+let branchMin = getElem("branchMinIn");
+let branchMax = getElem("branchMaxIn");
+let sizeMin = getElem("sizeMinIn");
+let sizeMax = getElem("sizeMaxIn");
+let scaleMin = getElem("scaleMinIn");
+let scaleMax = getElem("scaleMaxIn");
+let angleMin = getElem("angleMinIn");
+let angleMax = getElem("angleMaxIn");
+let locX = getElem("locXIn");
+let locY = getElem("locYIn");
+let startLength = getElem("startLengthIn");
+let startWidth = getElem("startWidthIn");
+
+let colorRange = getElem("colorRangeIn");
+let frameTime = getElem("fpsIn");
+let redrawTime = getElem("redrawIn");
+
+// Recursive properties
+let nextStack = [];
+let busy;
+let curInterval = 0;
+let fps;
+let maxStack = 1;
+
+// Shortcut for getting html element
 function getElem(x) {
 	return document.getElementById(x);
 }
+// Util for keeping rgb values in the range [0-255]
 function clampColor(x) {
 	return Math.min(255,Math.max(0,x));;
 }
+// Hides the left side panel
+function hidePanel() {
+	sidebar.style.display = "none";
+	showButton.style.display = "block";
+}
+// Shows the left side panel
+function showPanel() {
+	sidebar.style.display = "block";
+	showButton.style.display = "none";
+}
 
-var canvas = getElem("canvas");
-var g2d = canvas.getContext("2d");
-var r,g,b;
-
-var minMaxLabels = ["branch","size","scale","angle"];
-var otherLabels = ["locX","locY","startLength","startWidth","colorRange"];
-
-var branchMin = getElem("branchMinIn");
-var branchMax = getElem("branchMaxIn");
-var sizeMin = getElem("sizeMinIn");
-var sizeMax = getElem("sizeMaxIn");
-var scaleMin = getElem("scaleMinIn");
-var scaleMax = getElem("scaleMaxIn");
-var angleMin = getElem("angleMinIn");
-var angleMax = getElem("angleMaxIn");
-var locX = getElem("locXIn");
-var locY = getElem("locYIn");
-var startLength = getElem("startLengthIn");
-var startWidth = getElem("startWidthIn");
-
-var colorRange = getElem("colorRangeIn");
-
-var nextStack = [];
-var blocking;
-var curInterval = 0;
-var fps;
-var maxStack = 1;
-
+// Updates an html label value
 function updateValue(element,value) {
-	getElem(element).innerHTML=value;
+	getElem(element).innerHTML = value;
 }
 
-function updateMinMaxValue(element,value) {
-	updateValue(element,value);
-	var isMin = element.toLowerCase().indexOf("min") > 0;
-	checkRanges(isMin);
-}
-
-function checkRanges(isMin) {
-	isMin = isMin || false;
-	minMaxLabels.forEach(function(x) {
-		var min = parseInt(getElem(x+"MinIn").value);
-		var max = parseInt(getElem(x+"MaxIn").value);
-		if(min > max) {
-			if(isMin) {
-				getElem(x+"Max").innerHTML=min;
-				getElem(x+"MaxIn").value=min;
-			} else {
-				getElem(x+"Min").innerHTML=max;
-				getElem(x+"MinIn").value=max;
-			}
+// Updates an html label and checks min/max range
+function updateMinMaxValue(ele,value) {
+	updateValue(ele,value);
+	
+	// Whether to set value based on min or max limit
+	let isMin = ele.indexOf("Min") > 0;
+	let baseEle = isMin? ele.substr(0,ele.indexOf("Min")) : ele.substr(0,ele.indexOf("Max"));
+	let min = parseInt(getElem(baseEle+"MinIn").value);
+	let max = parseInt(getElem(baseEle+"MaxIn").value);
+	
+	if(min > max) {
+		if(isMin) {
+			getElem(baseEle+"Max").innerHTML=min;
+			getElem(baseEle+"MaxIn").value=min;
+		} else {
+			getElem(baseEle+"Min").innerHTML=max;
+			getElem(baseEle+"MinIn").value=max;
 		}
-	});
+	}
 }
 
+// Updates all html labels with their input values
 function updateAllValues() {
 	minMaxLabels.forEach(function(x) {
-		getElem(x+"Min").innerHTML = getElem(x+"MinIn").value;
-		getElem(x+"Max").innerHTML = getElem(x+"MaxIn").value;
+		updateMinMaxValue(x+"Min",getElem(x+"MinIn").value);
+		updateMinMaxValue(x+"Max",getElem(x+"MaxIn").value);
 	});
 	otherLabels.forEach(function(x) {
-		getElem(x).innerHTML = getElem(x+"In").value;
+		updateValue(x,getElem(x+"In").value);
 	});
 }
 
+// Starts the generation of a tree
 function render() {
+	// Resizes the canvas to fit the screen
 	getElem("canvas").width = document.body.clientWidth-20;
 	getElem("canvas").height = document.body.clientHeight-20;
+	
+	// Stop any current tree being drawn
 	if(curInterval != 0) {
 		clearInterval(curInterval);
 	}
 	nextStack = [];
 	maxStack = 1;
+	
+	// Prepare the graphics canvas
 	g2d.clearRect(0,0,canvas.width,canvas.height);
 	g2d.strokeStyle="black";
 	g2d.lineCap="round";
@@ -86,20 +113,39 @@ function render() {
 	g = parseInt(Math.random()*200)+55;
 	b = parseInt(Math.random()*200)+55;
 	
-	recurse(locX.value,canvas.height-locY.value,startLength.value,startWidth.value,Math.PI/2,0, 1);
-	blocking = false;
-	curInterval = setInterval(nextFrame, 30);
+	// Create the first branch
+	let trunk = {
+		x: locX.value,
+		y: canvas.height-locY.value,
+		length: startLength.value,
+		width: startWidth.value,
+		angle: Math.PI/2,
+		dir: 0,
+		size: 1,
+	};
+	generateBranch(trunk);
+	
+	// Interval timer to continuously build the tree
+	busy = false;
+	curInterval = setInterval(nextFrame, frameTime.value);
+	
+	// Timer for drawing a new tree
+	if(redrawTime.value > 0) {
+		setTimeout(render, redrawTime.value*1000);
+	}
 }
 
-var nextFrame = function() {
-	if(!blocking) {
-		blocking = true;
-		var curStack;
-		var total = nextStack.length;
-		var limit = 12;
+// Function called from the interval timer, generates a batch of branches at a time
+let nextFrame = function() {
+	// Prevents concurrent modification
+	if(!busy) {
+		busy = true;
+		let curStack;
+		let total = nextStack.length;
+		let limit = 121-frameTime.value;
 		if(total > limit) {
 			curStack = nextStack.slice(0,limit);
-			for (var i = 0; i < total-limit; i++) {
+			for (let i = 0; i < total-limit; i++) {
 				nextStack[i] = nextStack[i+limit];
 			}
 			nextStack.length = total-limit;
@@ -108,45 +154,49 @@ var nextFrame = function() {
 			nextStack = [];
 		}
 		
-		var working;
+		let feedback;
 		if(total >= maxStack) {
 			maxStack = total;
-			working = "...";
+			feedback = "...";
 		} else {
-			working = parseInt((1-total/maxStack)*50+50);
+			feedback = parseInt((1-total/maxStack)*50+50)+"%";
 		}
 		
-		working += "% (fps:"+parseInt(1000 / ((new Date) - fps))+")";
+		feedback += " (fps:"+parseInt(1000 / ((new Date) - fps))+")";
 		
 		if(total == 0) {
 			updateValue("progress","");
 		} else {
-			updateValue("progress",working+"  "+total+" branches");
+			updateValue("progress",feedback+"  "+total+" branches");
 		}
 		
-		for(var i = 0; i < curStack.length; i++) {
-			recurse(curStack[i][0],curStack[i][1],curStack[i][2],curStack[i][3],curStack[i][4],curStack[i][5],curStack[i][6]);
-		}
-		blocking = false;
+		curStack.forEach(function(branch) {
+			generateBranch(branch);
+		});
+		busy = false;
 	}
 	fps = new Date;
 }
 
-function recurse(rootX, rootY, preLength, preWidth, preAngle, dir, preSize) {
-	var length = getLength(preLength);
-	var width = getLength(preWidth);
-	var angle = getAngle(preAngle, dir);
+// Generates a new branch from a previous one
+function generateBranch(root) {
+	// Calculate new branch properties based on previous
+	let length = getLength(root.length);
+	let width = getLength(root.width);
+	let angle = getAngle(root.angle, root.dir);
+	let dir = root.dir;
+	let branches = getBranches();
+	let size = getSize();
 	
-	var x = Math.cos(angle)*length+parseInt(rootX);
-	var y = -Math.sin(angle)*length+parseInt(rootY);
+	// Calculate new location based on length/angle
+	let x = Math.cos(angle)*length+parseInt(root.x);
+	let y = -Math.sin(angle)*length+parseInt(root.y);
 	
-	var branches = getBranches();
-	var colorScale = colorRange.value;
-	
-	var xr = parseInt((Math.random()*colorScale)-(colorScale/2));
-	var xg = parseInt((Math.random()*colorScale)-(colorScale/2));
-	var xb = parseInt((Math.random()*colorScale)-(colorScale/2));
-	var tr,tg,tb;
+	// Set randomized color by mode
+	let colorScale = colorRange.value;
+	let xr = parseInt((Math.random()*colorScale)-(colorScale/2));
+	let xg = parseInt((Math.random()*colorScale)-(colorScale/2));
+	let xb = parseInt((Math.random()*colorScale)-(colorScale/2));
 	
 	if(getElem("dynamicColor").checked) {
 		r = clampColor(r+xr);
@@ -159,55 +209,241 @@ function recurse(rootX, rootY, preLength, preWidth, preAngle, dir, preSize) {
 		g2d.strokeStyle = "rgb("+r+", "+g+", "+b+")";
 	}
 	
+	// Draw on 2d graphics canvas
 	g2d.beginPath();
-	g2d.moveTo(rootX,rootY);
+	g2d.moveTo(root.x,root.y);
 	g2d.lineWidth = parseInt(width);
 	g2d.lineTo(x,y,length);
 	g2d.stroke();
 	
+	// Initial vertical trunk
 	if(dir == 0) { dir = 1; }
 	
-	if(length < 2 || getSize() < preSize) { return; }
+	// Break out if new branch is too short or total branches too long
+	if(length < 3 || root.size > size) { return; }
 	
-	for(var i = 0; i < branches; i++) {
-		nextStack[nextStack.length] = [x,y,length,width,angle,dir,preSize+1];
+	// Else spawn new branches from this one, alternating the direction
+	for(let i = 0; i < branches; i++) {
+		let newBranch = {
+			x: x,
+			y: y,
+			length: length,
+			width: width,
+			angle: angle,
+			dir: dir,
+			size: root.size+1,
+		};
+		nextStack.push(newBranch);
 		dir *= -1;
 	}
 }
 
-function getLength(preLength) {
-	return Math.max(((Math.random()*(parseInt(scaleMax.value)-parseInt(scaleMin.value))+parseInt(scaleMin.value))*preLength/100),1);
+/*	Functions for generating the next branch properties.
+	Gets a value randomly selected between the min and max limits. */
+	
+function getRandomInRange(min,max) {
+	return Math.round(Math.random()*(parseInt(max.value)-parseInt(min.value))+parseInt(min.value));
 }
-
-function getAngle(preAngle, dir) {
-	if(dir == 0) {
-		return preAngle;
-	}
-	return ((Math.random()*(parseInt(angleMax.value)-parseInt(angleMin.value))+parseInt(angleMin.value))/100*Math.PI*dir + preAngle);
+function getLength(prevLength) {
+	return (getRandomInRange(scaleMin,scaleMax)/100*prevLength);
 }
-
+function getAngle(prevAngle, dir) {
+	if(dir == 0) { return prevAngle; }
+	return (getRandomInRange(angleMin,angleMax)/100*Math.PI*dir + prevAngle);
+}
 function getBranches() {
-	return Math.round(Math.random()*(parseInt(branchMax.value)-parseInt(branchMin.value))+parseInt(branchMin.value));
+	return getRandomInRange(branchMin,branchMax);
 }
-
 function getSize() {
-	return (Math.random()*(parseInt(sizeMax.value)-parseInt(sizeMin.value))+parseInt(sizeMin.value));
+	return getRandomInRange(sizeMin,sizeMax);
 }
 
-function onLoad() {
+/* Preset functions for specific types of trees */
+function defaultSettings() {
+	locX.value = document.body.clientWidth/2;
+	startLength.value = document.body.clientHeight/3;
+	locY.value = 20;
+	frameTime.value = 20;
+	redrawTime.value = 0;
+}
+
+function defaultTree() {
+	defaultSettings();
+	
 	branchMin.value = 2;
-	branchMax.value = 3;
+	branchMax.value = 4;
 	sizeMin.value = 6;
-	sizeMax.value = 14;
+	sizeMax.value = 16;
 	scaleMin.value = 66;
 	scaleMax.value = 80;
 	angleMin.value = 10;
 	angleMax.value = 25;
-	locX.value = 900;
-	locY.value = 20;
-	startLength.value = 260;
 	startWidth.value = 16;
-	colorRange.value = 20;
+	colorRange.value = 12;
+	
+	updateAllValues();
+	render();
+}
+
+function squareTree() {
+	defaultSettings();
+	
+	branchMin.value = 2;
+	branchMax.value = 2;
+	sizeMin.value = 10;
+	sizeMax.value = 10;
+	scaleMin.value = 68;
+	scaleMax.value = 68;
+	angleMin.value = 50;
+	angleMax.value = 50;
+	locY.value = 200;
+	startLength.value = 400;
+	startWidth.value = 16;
+	colorRange.value = 80;
+	frameTime.value = 60;
+	
+	updateAllValues();
+	render();
+}
+
+function lankyTree() {
+	defaultSettings();
+	
+	branchMin.value = 2;
+	branchMax.value = 3;
+	sizeMin.value = 6;
+	sizeMax.value = 12;
+	scaleMin.value = 66;
+	scaleMax.value = 80;
+	angleMin.value = 0;
+	angleMax.value = 14;
+	startLength.value = document.body.clientHeight/4;
+	startWidth.value = 10;
+	colorRange.value = 12;
+	
+	updateAllValues();
+	render();
+}
+
+function bushyTree() {
+	defaultSettings();
+	
+	branchMin.value = 2;
+	branchMax.value = 2;
+	sizeMin.value = 12;
+	sizeMax.value = 16;
+	scaleMin.value = 78;
+	scaleMax.value = 88;
+	angleMin.value = 12;
+	angleMax.value = 18;
+	locY.value = 60;
+	startLength.value = document.body.clientHeight/5;
+	startWidth.value = 10;
+	colorRange.value = 12;
+	
+	updateAllValues();
+	render();
+}
+
+function sproutTree() {
+	defaultSettings();
+	
+	branchMin.value = 2;
+	branchMax.value = 3;
+	sizeMin.value = 3;
+	sizeMax.value = 6;
+	scaleMin.value = 64;
+	scaleMax.value = 84;
+	angleMin.value = 5;
+	angleMax.value = 25;
+	startLength.value = document.body.clientHeight/4;
+	startWidth.value = 6;
+	colorRange.value = 12;
+	
+	updateAllValues();
+	render();
+}
+
+function shrubTree() {
+	defaultSettings();
+	
+	branchMin.value = 4;
+	branchMax.value = 4;
+	sizeMin.value = 6;
+	sizeMax.value = 8;
+	scaleMin.value = 80;
+	scaleMax.value = 88;
+	angleMin.value = 4;
+	angleMax.value = 20;
+	startLength.value = document.body.clientHeight/6;
+	startWidth.value = 18;
+	colorRange.value = 12;
+	
+	updateAllValues();
+	render();
+}
+
+function cartoonTree() {
+	defaultSettings();
+	
+	branchMin.value = 1;
+	branchMax.value = 2;
+	sizeMin.value = 10;
+	sizeMax.value = 10;
+	scaleMin.value = 58;
+	scaleMax.value = 76;
+	angleMin.value = 48;
+	angleMax.value = 50;
+	locY.value = 60;
+	startLength.value = document.body.clientHeight/2;
+	startWidth.value = 24;
+	colorRange.value = 52;
+	
+	updateAllValues();
+	render();
+}
+
+function artTree() {
+	defaultSettings();
+	
+	branchMin.value = 3;
+	branchMax.value = 4;
+	sizeMin.value = 6;
+	sizeMax.value = 16;
+	scaleMin.value = 92;
+	scaleMax.value = 92;
+	angleMin.value = 28;
+	angleMax.value = 44;
+	startLength.value = document.body.clientHeight/4;
+	startWidth.value = 50;
+	colorRange.value = 60;
+	frameTime.value = 60;
+	
+	updateAllValues();
+	render();
+}
+
+function randomTree() {
+	defaultSettings();
+	
+	function randomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+	
+	branchMin.value = randomInt(branchMin.min,branchMin.max);
+	branchMax.value = randomInt(branchMin.value,branchMax.max);
+	sizeMin.value = randomInt(sizeMin.min,sizeMin.max);
+	sizeMax.value = randomInt(sizeMin.value,sizeMax.max);
+	scaleMin.value = randomInt(scaleMin.min,scaleMin.max);
+	scaleMax.value = randomInt(scaleMin.value,scaleMax.max);
+	angleMin.value = randomInt(angleMin.min,angleMin.max);
+	angleMax.value = randomInt(angleMin.value,angleMax.max);
+	startLength.value = randomInt(startLength.min,startLength.max);
+	startWidth.value = randomInt(startWidth.min,startWidth.max);
+	colorRange.value = randomInt(colorRange.min,colorRange.max);
+	frameTime.value = 5;
 	
 	updateAllValues();
 	render();
